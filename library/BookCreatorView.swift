160d4f8fe27d981d56c10671703cea20b315726c
devMonkey87 <jlandetemunoz@icloud.com>
2023-10-31 18:54:35 +0100

    add small changes

--- library/BookCreatorView.swift
+++ library/BookCreatorView.swift
@@ -9,42 +9,49 @@ import SwiftUI
 
 struct BookCreatorView: View {
     @Environment(\.managedObjectContext) private var viewContext
-      @Binding var isPresented: Bool
-      @State private var bookTitle = ""
-      
-      var body: some View {
-          NavigationView {
-              Form {
-                  Section {
-                      TextField("Título del libro", text: $bookTitle)
-                  }
-              }
-              .navigationTitle("Agregar Libro")
-              .navigationBarItems(
-                  leading: Button("Cancelar") {
-                      isPresented = false
-                  },
-                  trailing: Button("Guardar") {
-                      if !bookTitle.isEmpty {
-                          let newBook = Book(context: viewContext)
-                          newBook.title = bookTitle
-                          do {
-                              try viewContext.save()
-                              isPresented = false // Cierra la vista modal
-                          } catch {
-                              // Manejar errores de guardado
-                              print("Error al guardar: \(error)")
-                          }
-                      }
-                  }
-                  .disabled(bookTitle.isEmpty)
-              )
-          }
-      }
-  }
+    @State private var bookTitle = ""
+    
+    
+    var body: some View {
+        NavigationView {
+            
+            
+            Form {
+                Section {
+                    TextField("Título del libro", text: $bookTitle)
+                }
+                Section {
+                    TextField("Autor", text: $bookTitle)
+                }
+            }
+            .navigationTitle("Agregar Libro")
+            .navigationBarItems(
+                leading: Button(action: {
+                    
+                }) {
+                    Image(systemName: "chevron.backward")
+                    
+                },
+                trailing: Button("Guardar") {
+                    if !bookTitle.isEmpty {
+                        let newBook = Book(context: viewContext)
+                        newBook.title = bookTitle
+                        do {
+                            try viewContext.save()
+                        } catch {
+                            // Manejar errores de guardado
+                            print("Error al guardar:       \(error)")
+                        }
+                    }
+                }
+                    .disabled(bookTitle.isEmpty)
+            )
+        }
+    }
+}
 
-  struct AddBookView_Previews: PreviewProvider {
-      static var previews: some View {
-          BookCreatorView(isPresented: .constant(true))
-      }
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookCreatorView()
+    }
 }
